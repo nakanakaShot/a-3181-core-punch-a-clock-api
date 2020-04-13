72e1c419160b50a9d8d01f44c26824f5b17cce9b
mit-uchida <f-uchida@k-mit.jp>
2020-04-13 11:18:25 +0900

    CICDの様子を見るためわざとコケるテストを実装

---
+++ src/test/java/com/herokuapp/a3181core/punchaclockdev/SampleTests.java
@@ -0,0 +1,33 @@
+package com.herokuapp.a3181core.punchaclockdev;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SampleTests {
+
+    static Stream<Arguments> demoProvider(){
+        return Stream.of(
+                Arguments.of(1,1,2),
+                Arguments.of(2,2,5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("demoProvider")
+    @DisplayName("Parameterizedテストのデモ")
+    void demo(int i, int j, int expected){
+        Assertions.assertEquals(expected,i+j);
+    }
+
+    @Test
+    void fail(){
+        Assertions.fail();
+    }
+}
