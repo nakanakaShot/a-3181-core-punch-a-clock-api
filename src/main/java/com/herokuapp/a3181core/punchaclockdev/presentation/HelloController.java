c51f5d24430020c01d941d77021fcdcdfe5a1d36
mamiya999 <tisemihu@ichigo.me>
2020-04-13 18:44:54 +0900

    application.ymlのパッケージ場所変更、port8080を明示、レビュー指摘対応

--- src/main/java/com/herokuapp/a3181core/punchaclockdev/presentation/HelloController.java
+++ src/main/java/com/herokuapp/a3181core/punchaclockdev/presentation/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
 public class HelloController {
 
   @RequestMapping(method = RequestMethod.GET)
