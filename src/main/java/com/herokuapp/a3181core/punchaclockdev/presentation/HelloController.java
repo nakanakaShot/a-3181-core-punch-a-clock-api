71dae7eb4c6c1e27a131da65b68d4889572c5fec
mamiya999 <tisemihu@ichigo.me>
2020-04-14 14:41:03 +0900

    Hello Worldの次に現在時刻を表示

--- src/main/java/com/herokuapp/a3181core/punchaclockdev/presentation/HelloController.java
+++ src/main/java/com/herokuapp/a3181core/punchaclockdev/presentation/HelloController.java
@@ -1,5 +1,7 @@
 package com.herokuapp.a3181core.punchaclockdev.presentation;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String hello() {
-        return "Hello World";
+
+        return "Hello World, Now " + now.format(timeFormat);
     }
 
 }
