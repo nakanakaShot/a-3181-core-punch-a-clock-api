2b1486411136c3e259bcf3449283c4ef48e88db7
mit-uchida <f-uchida@k-mit.jp>
2020-04-20 14:34:02 +0900

    lombok.Valueは相性が悪い(多分equalsとかhashCodeのせい)

--- src/main/java/com/herokuapp/a3181core/punchaclockdev/configure/AppProperties.java
+++ src/main/java/com/herokuapp/a3181core/punchaclockdev/configure/AppProperties.java
@@ -2,26 +2,29 @@ package com.herokuapp.a3181core.punchaclockdev.configure;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "app")
 @ConstructorBinding
-@Value
+@AllArgsConstructor
+@Getter
 @Validated
 public class AppProperties {
 
     @NotNull
-    private Slack slack;
+    private final Slack slack;
 
-    @Value
+    @AllArgsConstructor
+    @Getter
     public static class Slack {
 
         @NotBlank
-        private String appToken;
+        private final String appToken;
         @NotBlank
-        private String channelPostedToId;
+        private final String channelPostedToId;
     }
 }
