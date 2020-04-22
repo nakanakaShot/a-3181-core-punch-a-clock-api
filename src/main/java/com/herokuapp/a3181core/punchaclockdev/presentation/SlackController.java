c6bf4f6510db7955e8b05f326cb54a26dff75660
mit-uchida <f-uchida@k-mit.jp>
2020-04-22 17:48:43 +0900

    checkstyle違反を修正

--- src/main/java/com/herokuapp/a3181core/punchaclockdev/presentation/SlackController.java
+++ src/main/java/com/herokuapp/a3181core/punchaclockdev/presentation/SlackController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SlackController {
 
+    /**
+     * 出勤コメントを返すAPI
+     *
+     * @return 固定値
+     */
     @RequestMapping(path = "/slack/attend")
     public String attend() {
         return "出勤！おはようございます\uD83C\uDF1E";
