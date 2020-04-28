4d1f05e430a1f3decf535273900181f425dd11aa
ShortyN1969 <november.snp90@gmail.com>
2020-04-28 18:01:09 +0900

    checkstyle対応

--- src/test/java/com/herokuapp/a3181core/punchaclockdev/shared/SlackAuthenticatorTest.java
+++ src/test/java/com/herokuapp/a3181core/punchaclockdev/shared/SlackAuthenticatorTest.java
@@ -135,7 +135,6 @@ class SlackAuthenticatorTest {
 
     @Test
     void isSuccessfullyUrlEncoded() {
-        String expected = "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c";
 
         Map<String, String[]> body = new LinkedHashMap<>();
         body.put("token", new String[]{"xyzz0WbapA4vBCDEFasx0q6G"});
@@ -154,6 +153,8 @@ class SlackAuthenticatorTest {
 
         MockHttpServletRequest input = new MockHttpServletRequest();
         input.setParameters(body);
+
+        String expected = "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c";
 
         Assertions.assertEquals(expected, target.getRawRequestBodyFromParameter(input));
     }
