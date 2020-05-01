06cfd03f2347ae83e5d7b05f60f6e771fa05cd42
mamiya999 <k-narumi@k-mit.jp>
2020-05-01 13:50:33 +0900

    お試しHttpServletResponse

--- src/main/java/com/herokuapp/a3181core/punchaclockdev/infrastructure/SlackRepository.java
+++ src/main/java/com/herokuapp/a3181core/punchaclockdev/infrastructure/SlackRepository.java
@@ -3,16 +3,19 @@ package com.herokuapp.a3181core.punchaclockdev.infrastructure;
 import com.herokuapp.a3181core.punchaclockdev.configure.AppProperties;
 import com.herokuapp.a3181core.punchaclockdev.domain.model.SlackParam;
 import com.herokuapp.a3181core.punchaclockdev.shared.ClockProvider;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
-
+@ResponseBody
 @Repository
 @RequiredArgsConstructor
-public class SlackRepository {
+public class SlackRepository extends HttpServlet {
 
     private final RestTemplate restTemplate;
     private final AppProperties appProperties;
@@ -23,15 +26,16 @@ public class SlackRepository {
      *
      * @param slackParam : tokenやteamIdなど。詳細はslackParam.javaを参照
      */
-    public void postParam(SlackParam slackParam) {
+    public HttpServletResponse postParam(SlackParam slackParam) {
         MultiValueMap<String, String> requestParam = new LinkedMultiValueMap<>();
         requestParam.add("token", appProperties.getSlack().getAppToken());
         requestParam.add("channel", appProperties.getSlack().getChannelPostedToId());
         requestParam
             .add("text",
                 "現在時刻 " + clockProvider.nowAsFormatted() + " 名前 " + slackParam.getUserName());
-        restTemplate
-            .postForObject("https://slack.com/api/chat.postMessage", requestParam, String.class);
-
+        HttpServletResponse response = restTemplate
+            .postForObject("https://slack.com/api/chat.postMessage", requestParam,
+                HttpServletResponse.class);
+        return response;
     }
 }
